5fd203b9c1bb73bb97d35b2fd878eb0174537241
LordFLader <devyansh1kalra@gmail.com>
2022-12-31 16:51:09 +0530

    minor fixes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ import "../Assets/css/card.css"
 const Card = (props) => {
     
     return (
-        <div className={`cardContainer w-full flex justify-center items-center  ${props.reverse && 'flex-row-reverse'}`}>
+        <div className={`cardContainer w-full flex justify-center items-center ${props.reverse && 'flex-row-reverse'}`}>
             <div className='cardTextContainer mx-5'>
                 <span className='text-3xl md:text-5xl  sm:text-4xl text-white font-semibold '>{props.title}</span><br/><br/>
                 <span className='text-xl md:text-3xl sm:text-2xl  text-white '>{props.content}</span>
